feb64a049c63a9a51979687730f6a47034eb3aca
Javier Gomez <jgomez@twoconnect.com>
2015-12-01 23:39:20 -0500

    base

---
+++ Gruntfile.js
@@ -0,0 +1,124 @@
+/*!
+ * base-ui Gruntfile
+ * Copyright 2015 Andrew Castro
+ * Licensed under MIT (https://github.com/suiteappdev/base-ui/blob/master/LICENSE)
+ */
+
+module.exports = function(grunt) {
+  'use strict';
+
+  // Force use of Unix newlines
+  grunt.util.linefeed = '\n';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Metadata.
+    meta: {
+        distPath:       'dist/',
+        docsAssetsPath: 'docs/assets/',
+        docsDistPath:   'docs/dist/',
+        docsPath:       'docs/',
+        srcPath:        'sass/',
+    },
+
+    banner: '/*!\n' +
+            ' * =====================================================\n' +
+            ' * base-ui v<%= pkg.version %>\n' +
+            ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under <%= pkg.license %> (https://github.com/suiteappdev/base-ui/blob/master/LICENSE)\n' +
+            ' *\n' +
+            ' * v<%= pkg.version %> designed by @suiteappdev.\n' +
+            ' * =====================================================\n' +
+            ' */\n',
+
+    clean: {
+      dist: ['<%= meta.distPath %>/css', '<%= meta.docsDistPath %>/css']
+    },
+
+    sass: {
+      options: {
+        sourcemap: 'none',
+        style: 'expanded',
+        unixNewlines: true
+      },
+      core: {
+        src: 'sass/baseui.scss',
+        dest: '<%= meta.distPath %>css/<%= pkg.name %>.css'
+      }
+		},
+
+    usebanner: {
+      dist: {
+        options: {
+          position: 'top',
+          banner: '<%= banner %>'
+        },
+        files: {
+          src: [
+            '<%= meta.distPath %>css/*.css'
+          ]
+        }
+      }
+    },
+
+    copy: {
+      fonts: {
+        expand: true,
+        src: 'fonts/*',
+        dest: '<%= meta.distPath %>'
+      }
+    },
+
+    cssmin: {
+      options: {
+        keepSpecialComments: '*' // keep all important comments
+      },
+      docs: {
+        src: [
+          '<%= meta.docsAssetsPath %>css/docs.css',
+          '<%= meta.docsAssetsPath %>css/pygments-manni.css',
+          '<%= meta.docsAssetsPath %>css/normalize.css'
+        ],
+        dest: '<%= meta.docsAssetsPath %>css/docs.min.css'
+      }
+    },
+
+    watch: {
+      options: {
+        hostname: 'localhost',
+        livereload: true,
+        port: 8000
+      },
+      css: {
+        files: '<%= meta.srcPath %>**/*.scss',
+        tasks: ['dist-css', 'copy']
+      }
+    },
+    connect: {
+      site: {
+        options: {
+          base: 'dist/',
+          hostname: 'localhost',
+          keepalive: true,
+          livereload: true,
+          open: true,
+          port: 8000
+        }
+      }
+    }
+  });
+
+
+  // Load the plugins
+  require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
+  require('time-grunt')(grunt);
+
+  // Tasks
+  grunt.registerTask('dist-css', ['sass', 'usebanner', 'cssmin']);
+  grunt.registerTask('dist', ['clean', 'dist-css', 'copy']);
+  grunt.registerTask('server', ['dist','connect', 'watch']);
+
+  grunt.registerTask('default', ['dist']);
+};
